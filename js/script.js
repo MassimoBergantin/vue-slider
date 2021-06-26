75bcea00dd592f32254762f28a44c72099ca30f0
MassimoBergantin <maxbergax@gmail.com>
2021-06-26 16:31:04 +0200

    Slider

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 Vue.config.devtools = true;
+
+new Vue (
+    {
+        el: '#app',
+        data: {
+            pics: [
+                'img/brook.jpeg',
+                'img/NY.jpeg',
+                'img/Roma.jpeg',
+                'img/vegas.jpeg',
+            ],
+            picsIndex: 2,
+        },
+        methods: {
+            next: function () {
+                this.picsIndex++;
+            },
+            prev: function () {
+                this.picsIndex--;
+            }
+        }
+    }
+)
