a83524b48dc2c55dbcdea1d63e4cc8c24bd98c92
MassimoBergantin <maxbergax@gmail.com>
2021-06-29 17:55:31 +0200

    Slider

--- js/script.js
+++ js/script.js
@@ -12,13 +12,38 @@ new Vue (
             ],
             picsIndex: 2,
         },
+        created () {
+            setInterval(() => {
+
+                this.next();
+
+            }, 1500)
+        },
         methods: {
+
             next: function () {
-                this.picsIndex++;
+
+                if(this.picsIndex === (this.pics.length - 1)) {
+                    this.picsIndex = 0;                    
+                } else {
+                    this.picsIndex++;
+                }
             },
             prev: function () {
-                this.picsIndex--;
+                if (this.picsIndex === 0) {
+                    this.picsIndex = this.pics.length - 1;
+                } else {
+                    this.picsIndex--; 
+                }
+            },
+            currentDot : function (index) {
+                if (index === this.picsIndex) {
+                    return 'current' ;  
+                } else {
+                    return '';
+                }
             }
         }
     }
 )
+
